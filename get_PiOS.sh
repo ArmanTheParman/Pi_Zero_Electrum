80b4930004ad93da34878f079f759f99052408e8
Parman <armantheparman@gmail.com>
2023-10-02 23:25:23 +1100

    v2.0.0

---
+++ get_PiOS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "getting zip file"
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz
+
+echo "getting sha256 file"
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz.sha256
+
+echo "getting signature of hash file"
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz.sig
+
+echo "hashing"
+
+shasum -a 256 --ignore-missing --verify *.sig
+
+echo "fetching public key"
+gpg --import Rasberry_Pi.pubkey
+
+echo "verifying signature"
+gpg --verify *.sig
