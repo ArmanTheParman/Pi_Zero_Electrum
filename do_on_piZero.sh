0f4d3d87b98d48bec51f28118d882e45e93f3a1f
Parman <armantheparman@gmail.com>
2023-10-04 16:48:17 +1100

    need older version of buster

---
+++ do_on_piZero.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "HISTSIZE=0
+HISTFILESIZE=0
+" >> ~/.bashrc 
+
+
+clear
+echo " Have you done this?
+
+1. copy deb directroy to desktop
+2. copy Electrum tar file to desktop
+3. copy Mnemonic Code file to desktop
+
+Hit <enter> to continue
+"
+read
+
+cd ~Desktop
+tar -xvf *.gz
+rm -rf *.gz
+
+mv Ele* Electrum
+
+cd deb_files
+sudo dpkg -i *.deb
+
+cd ..
+rm -rf deb_files
+
+cat << EOF >./RUN_ELECTRUM
+#!/bin/bash
+echo "Please wait..."
+cd ~/Desktop/Electrum
+./run_electrum
+EOF
+
+sudo chmod +x ~/Desktop/run_electrum
+
+
+
+
+
+
--- get_PiOS.sh
+++ get_PiOS.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "getting zip file"
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-04-07/2022-04-04-raspios-buster-armhf.img.xz
 curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz
 
 echo "getting sha256 file"
-curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz.sha256
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-04-07/2022-04-04-raspios-buster-armhf.img.xz.sha256
 
 echo "getting signature of hash file"
-curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz.sig
+curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-04-07/2022-04-04-raspios-buster-armhf.img.xz.sig
 
 echo "hashing"
 
