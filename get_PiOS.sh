09603f502c379152f91fa5848d84af9e333b87a2
Parman <armantheparman@gmail.com>
2023-10-04 16:59:38 +1100

    smol bug

--- get_PiOS.sh
+++ get_PiOS.sh
@@ -2,7 +2,6 @@
 
 echo "getting zip file"
 curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-04-07/2022-04-04-raspios-buster-armhf.img.xz
-curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-09-26/2022-09-22-raspios-buster-armhf.img.xz
 
 echo "getting sha256 file"
 curl -LO https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2022-04-07/2022-04-04-raspios-buster-armhf.img.xz.sha256
